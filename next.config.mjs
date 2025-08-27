5c0c94e3bea58ea8f1fade835b1602baff87e0eb
Tejas <tejaschaudhari00001@gmail.com>
2025-08-27 18:54:50 +0530

    portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/PORTFOLIO1' : '',
-  assetPrefix: isProd ? '/PORTFOLIO1/' : '',
+  // output: 'export',
+  // basePath: isProd ? '/PORTFOLIO1' : '',
+  // assetPrefix: isProd ? '/PORTFOLIO1/' : '',
   images: {
     unoptimized: true,
   },
