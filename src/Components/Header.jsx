0d4ce9b940a1235f299801cb836a1735ab6196d6
Tejas <tejaschaudhari00001@gmail.com>
2025-08-28 10:02:41 +0530

    portfolio

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -17,29 +17,29 @@ const Header = () => {
     <>
       <div className={`flex fixed justify-between items-center backdrop-blur-sm bg-black/45 shadow-lg/20 shadow-blue-400 p-3 py-4.5 w-full sm:px-40 z-50 ${toggle ? 'inline-block' : 'flex'} md:${toggle ? 'flex' : 'flex'}`} >
         <div className="logo flex gap-3">
-          <p className='flex justify-center items-center text-3xl font-bold bg-gradient-to-r focus:from-blue-500 focus:to-blue-200 bg-clip-text text-transparent cursor-pointer'>Tejas Chaudhari</p>
+          <p className='flex justify-center items-center text-3xl font-bold bg-gradient-to-r active:from-blue-200 active:to-blue-500 from-blue-500 to-blue-200 bg-clip-text text-transparent cursor-pointer'>Tejas Chaudhari</p>
         </div>
         {!toggle ? <CgMenuRightAlt className='text-3xl font-bold cursor-pointer absolute right-4 top-3 md:hidden' onClick={toggleNav} /> :
           <CgMenuRight className='text-3xl font-bold cursor-pointer absolute right-4 top-3 md:hidden' onClick={toggleNav} />
         }
         <div className={`list space-y-1 space-x-3 md:space-y-0 mt-5 md:mt-0 md:flex w-full md:w-fit justify-center items-center hidden ${toggle ? 'inline-block' : 'hidden'}`}>
           <Link href="#home" className='flex justify-end text-center'>
-            <p className='bg-gradient-to-r focus:from-blue-500 focus:to-blue-400 md:hover:from-blue-500  md:hover:to-blue-400 px-2 py-0.5 rounded-2xl'>Home</p>
+            <p className='bg-gradient-to-r active:from-blue-500 active:to-blue-400 md:hover:from-blue-500  md:hover:to-blue-400 px-2 py-0.5 rounded-2xl'>Home</p>
           </Link>
           <Link href="#about" className='flex justify-end text-center'>
-            <p className='bg-gradient-to-r focus:from-blue-500 focus:to-blue-400 md:hover:from-blue-500 md:hover:to-blue-400 px-2 rounded-2xl'>About</p>
+            <p className='bg-gradient-to-r active:from-blue-500 active:to-blue-400 md:hover:from-blue-500 md:hover:to-blue-400 px-2 rounded-2xl'>About</p>
           </Link>
           <Link href="#A&E" className='flex justify-end text-center'>
-            <p className='bg-gradient-to-r focus:from-blue-500 focus:to-blue-400 md:hover:from-blue-500 md:hover:to-blue-400 px-2 rounded-2xl'>AchieveExperience</p>
+            <p className='bg-gradient-to-r active:from-blue-500 active:to-blue-400 md:hover:from-blue-500 md:hover:to-blue-400 px-2 rounded-2xl'>AchieveExperience</p>
           </Link>
           <Link href="#projects" className='flex justify-end text-center'>
-            <p className='bg-gradient-to-r focus:from-blue-500 focus:to-blue-400 md:hover:from-blue-500 md:hover:to-blue-400 px-2 rounded-2xl'>Projects</p>
+            <p className='bg-gradient-to-r active:from-blue-500 active:to-blue-400 md:hover:from-blue-500 md:hover:to-blue-400 px-2 rounded-2xl'>Projects</p>
           </Link>
           <Link href="#contact" className='flex justify-end text-center'>
-            <p className='bg-gradient-to-r focus:from-blue-500 focus:to-blue-400 md:hover:from-blue-500  md:hover:to-blue-400 px-2 rounded-2xl'>Connect</p>
+            <p className='bg-gradient-to-r active:from-blue-500 active:to-blue-400 md:hover:from-blue-500  md:hover:to-blue-400 px-2 rounded-2xl'>Connect</p>
           </Link>
           <Link href="" className='flex justify-end text-center'>
-            <button onClick={handleModal} className='bg-gradient-to-r from-blue-500 focus:from-blue-500 focus:to-blue-400 to-blue-400 rounded text-sm  md:hover:to-blue-500  md:hover:from-blue-400 transition duration-300 mx-2 px-4 py-1.5 cursor-pointer' suppressHydrationWarning={true}>Resume</button>
+            <button onClick={handleModal} className='bg-gradient-to-r from-blue-500 active:from-blue-500 active:to-blue-400 to-blue-400 rounded text-sm  md:hover:to-blue-500  md:hover:from-blue-400 transition duration-300 mx-2 px-4 py-1.5 cursor-pointer' suppressHydrationWarning={true}>Resume</button>
           </Link>
         </div>
       </div>
